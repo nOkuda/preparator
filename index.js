98c078a608fdc56a191d8a25cbce20d5ff128c7e
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2019-03-27 16:49:00 -0400

    Now works as React component

--- index.js
+++ index.js
@@ -1,45 +1,79 @@
-function loadAndConvertFile(files) {
-  const file = files[0];
-  const reader = new FileReader();
-  reader.onload = function(e) {
-    const displayArea = document.getElementById("display")
-    displayArea.innerHTML = convertText(e.target.result);
-  }
-  reader.readAsText(file);
-}
-
 function convertText(text) {
   // TODO perform conversion
   return text;
-}
-
-function saveDoc() {
-  const text = document.getElementById("display").value;
-  const blob = new Blob([text], {type: 'text/plain'});
-  const filename = "default.tess";
-  var downloadLink = document.createElement("a");
-  downloadLink.download = filename;
-  downloadLink.innerHTML = "Download File";
-  if (window.webkitURL != null)
-  {
-    // Chrome allows the link to be clicked
-    // without actually adding it to the DOM.
-    downloadLink.href = window.webkitURL.createObjectURL(blob);
-  }
-  else
-  {
-    // Firefox requires the link to be added to the DOM
-    // before it can be clicked.
-    downloadLink.href = window.URL.createObjectURL(blob);
-    downloadLink.onclick = destroyClickedElement;
-    downloadLink.style.display = "none";
-    document.body.appendChild(downloadLink);
-  }
-
-  downloadLink.click();
 }
 
 function destroyClickedElement(event)
 {
   document.body.removeChild(event.target);
 }
+
+class Preparator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ""};
+
+    this.loadAndConvertFile = this.loadAndConvertFile.bind(this);
+    this.postprocess = this.postprocess.bind(this);
+    this.updateText = this.updateText.bind(this);
+    this.saveDoc = this.saveDoc.bind(this);
+
+    this.fileInput = React.createRef();
+  }
+
+  loadAndConvertFile() {
+    const file = this.fileInput.current.files[0];
+    const reader = new FileReader();
+    reader.onload = this.postprocess;
+    reader.readAsText(file);
+  }
+
+  postprocess(e) {
+    this.setState({text: convertText(e.target.result)});
+  }
+
+  updateText(event) {
+    this.setState({text: event.target.value});
+  }
+
+  saveDoc() {
+    const text = this.state.text;
+    const blob = new Blob([text], {type: 'text/plain'});
+    const filename = "default.tess";
+    var downloadLink = document.createElement("a");
+    downloadLink.download = filename;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null)
+    {
+      // Chrome allows the link to be clicked
+      // without actually adding it to the DOM.
+      downloadLink.href = window.webkitURL.createObjectURL(blob);
+    }
+    else
+    {
+      // Firefox requires the link to be added to the DOM
+      // before it can be clicked.
+      downloadLink.href = window.URL.createObjectURL(blob);
+      downloadLink.onclick = destroyClickedElement;
+      downloadLink.style.display = "none";
+      document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+  }
+
+  render() {
+    return [
+      <div key="inputDiv"><input type="file" id="toBeConverted" ref={this.fileInput} onChange={this.loadAndConvertFile} /></div>,
+      <div key="displayDiv"><textarea id="display" value={this.state.text} onChange={this.updateText}/></div>,
+      <div key="saveDiv"><button type="button" onClick={this.saveDoc}>Save</button></div>
+    ];
+  }
+}
+
+const element = <Preparator />;
+
+ReactDOM.render(
+  element,
+  document.getElementById("tesseraePreparator")
+);
