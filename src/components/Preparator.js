bc1fa0b73be9e890aa6b882425316b1e2dad2184
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2019-04-05 19:01:03 -0400

    Added fast-xml-parser
    Also fixed package.json

--- src/components/Preparator.js
+++ src/components/Preparator.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import fxp from 'fast-xml-parser';
 
 function convertText(text) {
   // TODO perform conversion
-  return text;
+  /*
+  const parser = new DOMParser();
+  const dom = parser.parseFromString(text, 'application/xml');
+  console.log(dom.doctype);
+  console.log(Array.from(dom.firstElementChild.children).map(x => x.localName));
+  console.log(dom.firstElementChild.firstElementChild.innerHTML);
+  return Array.from(dom.children).map(x => x.localName).join('\n');
+  */
+  // TODO Idea:  use loose xml parser and correct bad stuff afterwards
+  // (fast-xmlparser? xml2js?)
+  const fxpOptions = {
+    attributeNamePrefix : "@_",
+    attrNodeName: "attr", //default is 'false'
+    ignoreAttributes: false
+  };
+  const parsedObj = fxp.parse(text, fxpOptions);
+  console.log(parsedObj);
+  // TODO Idea:  try replacing all bad stuff before parsing with DOM (regex)
+  // TODO Idea:  build validating xml parser that downloads external DTDs
+  // (sax-js)
+  // TODO firefox refuses to finish parsing once it finds the first error;
+  // chrome parses along anyway; DTD problem?
 }
 
 function destroyClickedElement(event)
@@ -13,7 +35,7 @@ function destroyClickedElement(event)
 class Preparator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {text: ""};
+    this.state = {text: ''};
 
     this.loadAndConvertFile = this.loadAndConvertFile.bind(this);
     this.postprocess = this.postprocess.bind(this);
@@ -41,10 +63,10 @@ class Preparator extends React.Component {
   saveDoc() {
     const text = this.state.text;
     const blob = new Blob([text], {type: 'text/plain'});
-    const filename = "default.tess";
-    var downloadLink = document.createElement("a");
+    const filename = 'default.tess';
+    var downloadLink = document.createElement('a');
     downloadLink.download = filename;
-    downloadLink.innerHTML = "Download File";
+    downloadLink.innerHTML = 'Download File';
     if (window.webkitURL != null)
     {
       // Chrome allows the link to be clicked
@@ -57,7 +79,7 @@ class Preparator extends React.Component {
       // before it can be clicked.
       downloadLink.href = window.URL.createObjectURL(blob);
       downloadLink.onclick = destroyClickedElement;
-      downloadLink.style.display = "none";
+      downloadLink.style.display = 'none';
       document.body.appendChild(downloadLink);
     }
 
