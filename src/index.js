9bb6790ea79d1f2150b9c73e5f6d4c9592962e66
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2019-07-26 12:02:29 -0400

    Remove extraneous files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Preparator from './components/Preparator';
-
-const root = document.getElementById('root');
-
-render((
-  <Preparator />
-), root);
